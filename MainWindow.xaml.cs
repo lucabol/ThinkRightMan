2a92a31f10f7fb05acc7126b0d3924ab0498f98f
Luca Bolognese <lucabol@microsoft.com>
2017-10-06 16:49:42 +0200

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThinkRightMan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string[] _messages = File.ReadAllLines("Messages.txt");
+        static int _lenght = _messages.Length;
+        static int _count = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowMessage();
+        }
+
+        private void ShowMessage()
+        {
+            this.Message.Text = _messages[_count];
+            _count = (_count + 1) % _lenght;
+        }
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            ShowMessage();
+        }
+
+        private void Skip_Click(object sender, RoutedEventArgs e)
+        {
+            _count = 0;
+            ShowMessage();
+        }       
+    }
+}
